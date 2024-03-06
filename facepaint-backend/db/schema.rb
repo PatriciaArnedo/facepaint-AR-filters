8007934c4d9ea39ca1f1b176fe7dd9e169d992d8
patriciaarnedo <13356497+PatriciaArnedo@users.noreply.github.com>
2024-03-06 16:29:27 -0500

    refactored edit account component to be functional

--- facepaint-backend/db/schema.rb
+++ facepaint-backend/db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_23_001647) do
-
+ActiveRecord::Schema[7.0].define(version: 2021_01_23_001647) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,16 +18,16 @@ ActiveRecord::Schema.define(version: 2021_01_23_001647) do
     t.string "name"
     t.string "img"
     t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_filters_on_user_id"
   end
 
   create_table "save_filters", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "filter_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["filter_id"], name: "index_save_filters_on_filter_id"
     t.index ["user_id"], name: "index_save_filters_on_user_id"
   end
@@ -36,8 +35,8 @@ ActiveRecord::Schema.define(version: 2021_01_23_001647) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "avatar"
     t.string "name"
     t.string "bio"
--- facepaint-frontend/src/account/EditAccount.jsx
+++ facepaint-frontend/src/account/EditAccount.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
 import { getUser, updateUser } from "../redux/actions";
@@ -6,117 +6,105 @@ import EditAccountForm from "./EditAccountForm";
 import ImageUpload from "./ImageUpload";
 import { Button, Divider } from "@mui/material";
 
-class EditAccount extends React.Component {
-  //state to toggle the edit account form
-  state = {
-    beenClicked: false,
-    showModal: false,
+const EditAccount = ({ userId, userObj, getUser }) => {
+  const [beenClicked, setBeenClicked] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  // Fetch user data on component mount
+  useEffect(() => {
+    getUser(userId);
+  }, [userId, getUser]);
+
+  // Toggle state handlers
+  const clickHandler = () => {
+    setBeenClicked(!beenClicked);
   };
 
-  //action that gets user id when component mounts to make sure the user can edit their account
-  componentDidMount() {
-    this.props.getUser(this.props.userId);
-  }
-
-  //toggles state
-  clickHandler = () => {
-    this.setState({ beenClicked: !this.state.beenClicked });
+  const modalHandler = () => {
+    setShowModal(!showModal);
   };
 
-  //toggles state
-  modalHandler = () => {
-    this.setState({ showModal: !this.state.showModal });
-  };
-
-  render() {
-    return (
-      <div className="edit-account">
-        <div style={{ textAlign: "center" }}>
-          {/* checks for user avatar and if not found uses stock avatar */}
-          <img
-            src={
-              this.props.userObj.avatar
-                ? this.props.userObj.avatar
-                : "https://i.imgur.com/igyvLpE.jpg"
-            }
-            className="account-avatar"
-            alt="profile avatar"
-          />
-          <br />
-          <Button variant="contained" onClick={this.modalHandler}>
-            Upload Picture
-          </Button>
-          <br />
-          <br />
-          <NavLink to={`/my-profile`}>
-            <h3>View My Profile</h3>
-          </NavLink>
-        </div>
-        <Divider orientation="vertical" flexItem sx={{ marginRight: "70px" }} />{" "}
-        <div className="account-details">
-          <h2>My Account</h2>
-          {/* ternaries to display user information or edit form dependind on whether the edit form button was clicked and if user data has loaded */}
-          {this.props.userObj.username ? (
-            this.state.beenClicked ? (
-              <>
-                {/* If the edit button has been clicked the edit form will display */}
-                <p>
-                  <strong className="editable">Username:</strong> @
-                  {this.props.userObj.username}
-                </p>
-                <br />
-                <EditAccountForm clickHandler={this.clickHandler} />
-                <br />
-                <Button
-                  variant="contained"
-                  color="warning"
-                  onClick={this.clickHandler}
-                >
-                  Cancel
-                </Button>
-              </>
-            ) : (
-              <>
-                {/* If the edit button has not been clicked user information will display */}
-
-                <p>
-                  <strong className="editable">Username:</strong> @
-                  {this.props.userObj.username}
-                </p>
-                <br />
-                <p>
-                  <strong className="editable">Name: </strong>
-                  {this.props.userObj.name}
-                </p>
-                <br />
-                <p>
-                  <strong className="editable">Instagram:</strong>{" "}
-                  {this.props.userObj.instagram}
-                </p>
-                <br />
-                <p>
-                  <strong className="editable">Bio:</strong>{" "}
-                  <span className="account-bio">{this.props.userObj.bio}</span>
-                </p>
-                <br />
-                <Button variant="contained" onClick={this.clickHandler}>
-                  Edit
-                </Button>
-              </>
-            )
-          ) : (
-            <h3>Loading...</h3>
-          )}
-          {/* <ToggleButton checked={this.state.beenClicked} onChange={this.clickHandler} onLabel="Cancel" offLabel="Edit" /> */}
-          <br />
-          {this.state.showModal ? (
-            <ImageUpload cancelHandler={this.modalHandler} />
-          ) : null}
-        </div>
+  return (
+    <div className="edit-account">
+      <div style={{ textAlign: "center" }}>
+        <img
+          src={
+            userObj.avatar ? userObj.avatar : "https://i.imgur.com/igyvLpE.jpg"
+          }
+          className="account-avatar"
+          alt="profile avatar"
+        />
+        <br />
+        <Button variant="contained" onClick={modalHandler}>
+          Upload Picture
+        </Button>
+        <br />
+        <br />
+        <NavLink to={`/my-profile`}>
+          <h3>View My Profile</h3>
+        </NavLink>
       </div>
-    );
-  }
-}
+      <Divider
+        orientation="vertical"
+        flexItem
+        style={{ marginRight: "70px" }}
+      />
+      <div className="account-details">
+        <h2>My Account</h2>
+        {userObj.username ? (
+          beenClicked ? (
+            <>
+              <p>
+                <strong className="editable">Username:</strong> @
+                {userObj.username}
+              </p>
+              <br />
+              <EditAccountForm clickHandler={clickHandler} />
+              <br />
+              <Button
+                variant="contained"
+                color="warning"
+                onClick={clickHandler}
+              >
+                Cancel
+              </Button>
+            </>
+          ) : (
+            <>
+              <p>
+                <strong className="editable">Username:</strong> @
+                {userObj.username}
+              </p>
+              <br />
+              <p>
+                <strong className="editable">Name: </strong>
+                {userObj.name}
+              </p>
+              <br />
+              <p>
+                <strong className="editable">Instagram:</strong>{" "}
+                {userObj.instagram}
+              </p>
+              <br />
+              <p>
+                <strong className="editable">Bio:</strong>{" "}
+                <span className="account-bio">{userObj.bio}</span>
+              </p>
+              <br />
+              <Button variant="contained" onClick={clickHandler}>
+                Edit
+              </Button>
+            </>
+          )
+        ) : (
+          <h3>Loading...</h3>
+        )}
+        <br />
+        {showModal ? <ImageUpload cancelHandler={modalHandler} /> : null}
+      </div>
+    </div>
+  );
+};
 
 function msp(state) {
   return {
